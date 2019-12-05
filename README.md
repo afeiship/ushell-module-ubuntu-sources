01d9785a3f19c11088b14c66467979b74c08d2e9
afeiship <1290657123@qq.com>
2019-12-06 07:58:03 +0800

    add: source initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# ushell-module-ubuntu-sources
+> Ubuntu sources.
+
+## installation
+```shell
+npm i -S afeiship/ushell-module-ubuntu-sources
+```
+
+## resources
+- https://www.linuxidc.com/Linux/2018-08/153709.htm
---
+++ index.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+## path:
+ROOT_PATH=$(dirname $BASH_SOURCE);
+
+alias sources-backup='cp /etc/apt/sources.list /etc/apt/sources.list.bak';
+alias sources-edit='sudo vim /etc/apt/sources.list';
+alias sources-reload='sudo apt-get update && sudo apt-get upgrade'
+
+unset ROOT_PATH;
